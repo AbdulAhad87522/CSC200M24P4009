e497d5b3afd28dd1bec830bcdfcea821c793b8c1
AbdulAhad87522 <ahadbahi263@gmail.com>
2025-10-31 21:00:17 +0500

    .

--- Solitaire/Solitaire/obj/Debug/net8.0/Solitaire.AssemblyInfo.cs
+++ Solitaire/Solitaire/obj/Debug/net8.0/Solitaire.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Solitaire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47d3347bbc9b4488c608e0fce70bbc38f63aff1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a86784ddb246a50cf7f7c195d388316e06f40e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Solitaire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Solitaire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
