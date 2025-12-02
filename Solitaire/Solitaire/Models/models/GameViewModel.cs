ef0335dc53bde7d6716ccc5b470f0cc5f21ad1fb
AbdulAhad87522 <ahadbahi263@gmail.com>
2025-12-02 11:30:04 +0500

    ..

--- Solitaire/Solitaire/Models/models/GameViewModel.cs
+++ Solitaire/Solitaire/Models/models/GameViewModel.cs
@@ -18,3 +18,4 @@
         public int CardIndex { get; set; }
     }
 }
+        
--- Solitaire/Solitaire/obj/Debug/net8.0/Solitaire.AssemblyInfo.cs
+++ Solitaire/Solitaire/obj/Debug/net8.0/Solitaire.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Solitaire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d0f5817a486ea280eef4bb41b5ca2b010a9971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+943591d4ebcd6260c08813cdfa939611ce7e3ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Solitaire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Solitaire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
